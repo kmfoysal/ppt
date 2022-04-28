ccce5c8b51f07b9a5f89bd087026c5d92f6f2b40
K.M FOYSAL <contact.kmfoysal@gmail.com>
2022-04-28 17:59:21 +0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroAreaComponent } from './hero-area/hero-area.component';
+import { VisionComponent } from './vision/vision.component';
+import { CategoryComponent } from './category/category.component';
+import { BrandComponent } from './brand/brand.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroAreaComponent,
+    VisionComponent,
+    CategoryComponent,
+    BrandComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,

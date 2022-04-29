73f9c1a3f6b111102c22c9868d501d16eb1f5204
K.M FOYSAL <contact.kmfoysal@gmail.com>
2022-04-29 16:48:54 +0600

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HeroAreaComponent } from './hero-area/hero-area.component';
-import { VisionComponent } from './vision/vision.component';
-import { CategoryComponent } from './category/category.component';
 import { BrandComponent } from './brand/brand.component';
+import { CategoryComponent } from './category/category.component';
 import { ChartComponent } from './chart/chart.component';
+import { HeroAreaComponent } from './hero-area/hero-area.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { VisionComponent } from './vision/vision.component';
+import { WorkComponent } from './work/work.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { FooterComponent } from './footer/footer.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { FaqComponent } from './faq/faq.component';
+import { BuyTodayComponent } from './buy-today/buy-today.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +26,18 @@ import { ChartComponent } from './chart/chart.component';
     VisionComponent,
     CategoryComponent,
     BrandComponent,
-    ChartComponent
+    ChartComponent,
+    WorkComponent,
+    PortfolioComponent,
+    TestimonialComponent,
+    FooterComponent,
+    PresentationComponent,
+    FaqComponent,
+    BuyTodayComponent,
+    PricingComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, SlickCarouselModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
